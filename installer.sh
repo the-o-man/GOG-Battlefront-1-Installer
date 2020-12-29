e9b48a438374ecd08ba5eb5d7bb8cb9d550d1528
the-o-man <thomas.e.ohara@protonmail.com>
2020-12-29 13:42:15 +0100

    finalized structure

--- installer.sh
+++ installer.sh
@@ -2,4 +2,9 @@
 sudo apt update && sudo apt upgrade
 sudo apt install python3 python3-pip wine winetricks
 
+unzip files.zip
+
 python3 ./install_gui.py
+
+rm install_gui.py
+rm gog_bf1_installer.sh
