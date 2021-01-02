75294268ae43f014bf70d1b569fa91c6d6560783
the-o-man <thomas.e.ohara@protonmail.com>
2021-01-02 14:47:09 +0100

    Final version

--- installer.sh
+++ installer.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-sudo apt update && sudo apt upgrade
-sudo apt install python3 python3-pip wine winetricks
+unzip ./installation_files.zip
 
 python3 ./install_gui.py
+
+rm gog_bf1_installer.sh install_gui.py prerecs.sh wine_status.py apt_status.py
 
 update-desktop-database
